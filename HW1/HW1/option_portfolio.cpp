f8fbfb2517b8c1d88e2e8093aac7e7abc601cf6e
wbpark91 <wbpark91@gmail.com>
2017-10-06 15:41:57 +0900

    Print function modified

--- HW1/HW1/option_portfolio.cpp
+++ HW1/HW1/option_portfolio.cpp
@@ -21,8 +21,24 @@ void OptionPortfolio::setEvalDate(Date d) {
 }
 
 void OptionPortfolio::printInfo() {
+    std::cout << "Information of Individual Options" << std::endl;
+    std::cout << "=================================" << std::endl;
+    std::cout << "No." << "\t" << "Price" << "\t\t" << "Delta";
+    std::cout << "\t\t" << "Gamma" << "\t\t" << "Vega" << std::endl;
+    for (int i = 0; i < instrument_.size(); ++i) {
+        printf("%d \t", i+1);
+        printf("%.5f \t", instrument_[i] -> price());
+        printf("%.5f \t", instrument_[i] -> delta());
+        printf("%.5f \t", instrument_[i] -> gamma());
+        printf("%.5f \t", instrument_[i] -> vega());
+        std::cout << std::endl;
+    }
+    std::cout << "==========================" << std::endl;
+    std::cout << "Portfolio Value Evaluation" << std::endl;
+    std::cout << "==========================" << std::endl;
     std::cout << "Price: " << calc(&Option::price) << std::endl;
     std::cout << "Delta: " << calc(&Option::delta) << std::endl;
     std::cout << "Gamma: " << calc(&Option::gamma) << std::endl;
     std::cout << "Vega: " << calc(&Option::vega) << std::endl;
+    std::cout << "==========================" << std::endl;
 }
